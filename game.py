4b170821d1c6a2b2d5d860ebea1dd1786232f1eb
Kenny Grossman <mrkengrossman@gmail.com>
2019-07-29 19:30:13 -0400

    Revised method of parsing saleStrings to be more accurate

--- game.py
+++ game.py
@@ -1,61 +1,53 @@
+import re
+
 class Game:
     def __init__(self, saleStrings):
-        # saleStrings should come in the form of
-        # [0]{'SAVE 60%'} - Sale percent ***SOMETIMES NOT PROVIDED***
-        # [0/1]{'FIFA 19'} - Name of sale item
-        # [1/2]{'Full Game'} - ***SOMETIMES NOT PROVIDED***
-        # [2/3]{'PS4'} - Platform
-        # [3/4]{'$59.99'} - Normal Price
-        # [4/5]{'$23.99'} - Sale Price ***SOMETIMES NOT PROVIDED***
+        '''
+        saleStrings usually come in the form of:
+        [0]{'SAVE 60%'} - Sale percent ***SOMETIMES NOT PROVIDED***
+        [1]{'FIFA 19'} - Name of sale item
+        [2]{'Full Game'} - ***SOMETIMES NOT PROVIDED***
+        [3]{'PS4'} - Platform
+        [4]{'$59.99'} - Normal Price
+        [5]{'$23.99'} - Sale Price ***SOMETIMES NOT PROVIDED***
+        '''
 
-        i = 0
-        len(saleStrings) > 4
-        self.newPrice = "Unavailable"
+        self.onSale = True
 
-        ###[--OPTIONAL--]###
-        #Check for 'SAVE XX%' string and case where title of game could contain the word 'SAVE'
-        if "SAVE" in saleStrings[i] and len(saleStrings) > 4:
-            self.discountString = saleStrings[i].strip()
-            i+=1
-        else:
-            self.discountString = "NULL"
-
-        #Set title
-        self.title = saleStrings[i]
-        i+=1
-
-        #Check if expected number of strings exist
-        if len(saleStrings) > 4:
-            self.type = saleStrings[i]
-            i+=1
-        else:
-            self.type = "NULL"
-
-        #Set platform
-        if saleStrings[i].startswith("PS"):
-            self.platform = saleStrings[i]
-            i+=1
-        else:
-            self.platform = "NULL"
-
-        #Set oldPrice
-        #We could instead make "Unavailable" values into NULL values for easier datawork later?
-        if saleStrings[i].startswith("$") or saleStrings[i] == "Unavailable":
-            self.oldPrice = saleStrings[i].replace('$', '')
-            i+=1
-        else:
-            self.oldPrice = "NULL"
-        
-        #This index may not exist
         try:
-            self.newPrice = saleStrings[i].replace('$', '')
-        except:
-            self.newPrice = "NULL"
+            #Check top of list for 'SAVE XX%' using Regex...throw exception if match is not found
+            if re.match('^SAVE ([1-9][0-9]|[0-9])%', saleStrings[0]):
+                self.discountString = saleStrings.pop(0).strip()
+            else:
+                self.onSale = False
+                assert(self.onSale)
+
+            #Set title from top of list
+            self.title = saleStrings.pop(0)
+
+            #Grab salePrice from end of list
+            lastElement = saleStrings.pop()
+            if lastElement.startswith('$'):
+                self.newPrice = lastElement.replace('$', '')
+
+            #Grab oldPrice from end of list
+            lastElement = saleStrings.pop()
+            if lastElement.startswith('$'):
+                self.oldPrice = lastElement.replace('$', '')
+
+            #Grab platform from end of list
+            lastElement = saleStrings.pop()
+            self.platform = lastElement
+
+            #If there is a remaining element it will be the type
+            if saleStrings:
+                self.contentType = saleStrings.pop()
+            else:
+                self.contentType = 'NULL'
+        except Exception as e:
+            print(e)
+            print('Item is not on sale and will be skipped')
 
     def print_game_string(self):
-        print(self.discountString + ", " + self.title + ", " + self.type + ", " +
-         self.platform + ", " + self.newPrice + ", " + self.oldPrice)
-
-    def print_test_game_string(self):
-        print(self.type + ", " + self.title + ", " + self.discountString + ", " +
+        print(self.discountString + ", " + self.title + ", " + self.contentType + ", " +
          self.platform + ", " + self.newPrice + ", " + self.oldPrice)
